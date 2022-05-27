84b8bafc3d61cc6ae8049d4443f7eedbf1f1e08e
Vyacheslav <sipiq235467@gmail.com>
2022-05-27 15:07:46 +0300

    edit user add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,36 @@
 class UsersController < ApplicationController
+  before_action :check_current_user, only: %i[edit update destroy]
+
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(params.require(:user).permit(:nickname, :email))
+      redirect_to user_path(@user)
+    else
+      flash[:error] = 'Что-то пошло не так.'
+      render :edit
+    end
+  end
+
   def destroy
-    @user = User.find(params[:id])
     @user.meeting.destroy_all
     @user.destroy
     flash[:success] = 'Ваш аккаунт успешно удален.'
     redirect_to root_path
+  end
+
+  private
+
+  def check_current_user
+    @user = User.find(params[:id])
+    unless current_user == @user
+      flash[:error] = 'Что-то пошло не так.'
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resources :meetings, only: %i[index show new create]
-  resources :users, only: %i[show edit destroy]
+  resources :users, only: %i[show edit destroy update]
 
   root "meetings#index"
 end
