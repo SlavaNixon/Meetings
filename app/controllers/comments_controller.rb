d1e0743f49cbb67e9d54928a694165e6b7f1beb8
Vyacheslav <sipiq235467@gmail.com>
2022-06-06 09:10:23 +0300

    comment owner dont catch email about comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -47,13 +47,9 @@ class CommentsController < ApplicationController
   end
 
   def notify_subscribers(event, comment)
-    # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    all_emails = event.subscriptions.map(&:user_email) + [event.user.email]
   
-    # По адресам из этого массива делаем рассылку
-    # Как и в подписках, берём EventMailer и его метод comment с параметрами
-    # И отсылаем в том же потоке
-    all_emails.each do |mail|
+    all_emails.excluding(comment.user&.email).each do |mail|
       MeetingMailer.comment(event, comment, mail).deliver_now
     end
   end
