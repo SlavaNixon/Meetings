097c78a978c97b6ea718e8198526b5ff593b8d51
Vyacheslav <sipiq235467@gmail.com>
2022-07-15 04:28:45 +0300

    datepicker add

--- Gemfile
+++ Gemfile
@@ -54,5 +54,3 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
-
-gem "importmap-rails", "~> 1.1"
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "tempusdominus-bootstrap-4", to: "https://ga.jspm.io/npm:tempusdominus-bootstrap-4@5.39.2/build/js/tempusdominus-bootstrap-4.js"
