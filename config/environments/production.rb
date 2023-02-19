493bf74fb38fc05afb4dfd69476bac4316a672be
Vyacheslav Litvinov <v.litvinov@rubetek.com>
2023-02-20 01:39:32 +0300

    debugging production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.assets.digest = false
+  config.serve_static_assets = true
+  p config.assets.paths
+  10.times { p config.assets.prefix }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
