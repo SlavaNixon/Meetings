1416e45c0dd6e4d18479d0a3066b08045f25a3a8
Vyacheslav Litvinov <v.litvinov@rubetek.com>
2023-02-20 00:07:32 +0300

    some updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'search-meetings.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: ENV['HOST_URL']}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :tls => true,
